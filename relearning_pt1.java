ad3c42e7f8f7621bb5239444c7d59b3b2594e0f1
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-07-24 18:21:07 -0400

    first commit in 7 months lets go!

---
+++ relearning_pt1.java
@@ -0,0 +1,7 @@
+public class relearning_pt1 {
+    public static void main(String[] args) {
+        System.out.println("\nHola Mundo bello!\n");
+        System.out.print("Hello everyone!");
+        System.out.print("testing");
+    }
+}
