2708250044f25a6ffbb40cebde800da9782ceaf3
matarisvan <luismilan21@gmail.com>
2023-07-31 01:07:47 -0400

    added indention to end of code

--- relearning_pt1.java
+++ relearning_pt1.java
@@ -2,6 +2,6 @@ public class relearning_pt1 {
     public static void main(String[] args) {
         System.out.println("\nHola Mundo bello!\n");
         System.out.print("Hello everyone!");
-        System.out.print("testing");
+        System.out.print("testing\n");
     }
 }
