e0ed45a2d50a0bef29fb67ce3b41e458f2253750
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-07-31 22:39:06 -0400

    init_assignment

---
+++ mod2redo.java
@@ -0,0 +1,14 @@
+/*
+Write a program that calculates the energy needed to heat water from an initial temperature to a final temperature.
+Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperature of the water.
+The formula to use for this program is:
+Q = waterMass ( finalTemperature – initialTemperature ) x 4184
+waterMass is water weight in kilograms
+finalTemperature and initialTemperature are temperatures in Celsius
+Q is the results in Joules
+*/
+
+
+public class mod2redo {
+    
+}
