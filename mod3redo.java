9e0798c30d307691350bf17dd22b506cbadc48c1
matarisvan <luismilan21@gmail.com>
2023-08-05 17:22:19 -0400

    init_assignment

---
+++ mod3redo.java
@@ -0,0 +1,13 @@
+/*
+Write a program that is similar to the popular game titled “Rock-Paper-Scissors.”
+The program randomly generates the number of 1, 2, or 3 with 1 being Rock, 2 being Paper, and 3 being Scissors.
+Then, the program is to prompt the user to enter a value of 1, 2, or 3.
+Next, the program displays a clear readable message displaying both the computer’s selection, the user’s selection, and the results.
+*/
+
+public class mod3redo {
+    public static void main(String[] args) {
+
+    }
+    
+}
