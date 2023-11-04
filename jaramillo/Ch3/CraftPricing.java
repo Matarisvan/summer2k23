18552a0c302267967dac6ed6287c9874dbe507b6
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-11-04 13:22:24 -0400

    init_assignment

---
+++ jaramillo/Ch3/CraftPricing.java
@@ -0,0 +1,21 @@
+/*
+Caitlyn's Crafty Creations computes a retail price for each product as the cost of materials 
+plus $14 multiplied by the number of hours of work required to create the product, plus $6 shipping and handling. 
+Caitlyn’s Crafty Creations is having a semi-annual sale now, and everything is 25% off before shipping and handling:
+
+Retail price = 0.75 * (cost of materials + (14 * hours of work)) + 6
+
+Create a class that contains a main () method that prompts the user for the name of a product (for example, “woven purse”), 
+the cost of materials before discount and the number of hours of work required. 
+Pass the numeric data to a method that computes the retail price of the product and returns the computed value 
+to the main () method where the product name and retail price are displayed. Save the program as CraftPricing.java.
+
+Please submit the source code for your program along with a word document that includes screen shots of the compiled results and executed output.
+*/
+
+package jaramillo.Ch3;
+
+public class CraftPricing {
+
+    
+}
