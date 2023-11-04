f85b598ce408df16fb5ae38860ffb546896e513a
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-11-03 23:01:07 -0400

    get set methods

---
+++ jaramillo/Ch3/Employee.java
@@ -0,0 +1,36 @@
+package jaramillo.Ch3;
+
+public class Employee {
+
+    // fields
+    private int empNum;
+    private String empLN;
+    private String empFN;
+    private double empSalary;
+
+    // methods
+    public int getEmpNum() {
+        return empNum;
+    }
+    public void setEmpNum(int setEmp) {s
+        empNum = setEmp;
+    }
+    public String getEmpLN() {
+        return empLN;
+    }
+    public void setEmpLN(String name) {
+        empLN = name;
+    }
+    public String getEmpFN() {
+        return empFN;
+    }
+    public void setEmpFN(String name) {
+        empFN = name;
+    }
+    public double getEmpSalary() {
+        return empSalary;
+    }
+    public void setEmpSalary(double sal) {
+        empSalary = sal;
+    }
+}
---
+++ jaramillo/Ch3/SpaService.java
@@ -0,0 +1,20 @@
+package jaramillo.Ch3;
+
+public class SpaService {
+    private String serviceDesc;
+    private double price;
+
+    public String getServiceDesc() {
+        return serviceDesc;
+    }
+    public void setServiceDesc(String setServDesc) {
+        serviceDesc = setServDesc;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double setPrice) {
+        price = setPrice;
+    }
+    
+}
