4000df7e0145c651fade58ba93a17c4bd43c81fc
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-11-13 21:58:25 -0500

    composition and nested classes

--- jaramillo/Ch4/SchoolDemo.java
+++ jaramillo/Ch4/SchoolDemo.java
@@ -2,8 +2,15 @@ package jaramillo.Ch4;
 
 public class SchoolDemo {
     public static void main(String[] args) {
-        School mySchool = new School("Pendejo University", "3500 Hoyne", 31768, 229);
+        School mySchool = new School("Pendejo University", "69 Threesome Lane", 31768, 229);
         mySchool.display();
+        String caca = "tu eres un culo caca";
+        
+        String cacaca = "string data within quotes";
+        int culo = 21;
+        double jaram = 21.22; 
+        System.out.println(caca + cacaca + culo + jaram);
     }
     
 }
+
