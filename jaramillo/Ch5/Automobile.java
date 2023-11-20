ee310ae3f525149e39cdd49d5e7f983a3a673387
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-11-19 22:03:25 -0500

    can display automobile and its data

--- jaramillo/Ch5/Automobile.java
+++ jaramillo/Ch5/Automobile.java
@@ -1,18 +1,18 @@
 /*
-Create an Automobile class for a dealership.
-Include fields for an ID number, make, model, color, year, vin number, miles per gallon, and speed.
-Include get and set methods for each field.
-Do not allow the ID to be negative or more than 9999; if it is, set the ID to 0.
-Do not allow the year to be earlier than 2000 or later than 2017; if it is, set the year to 0.
-Do not allow the miles per gallon to be less than 10 or more than 60; if it is, set the miles per gallon to 0.
-Car speed should be initialized as 0.
-Include a constructor that accepts arguments for each field value and uses the set methods to assign the values.
-Also write two methods, Accelerate () and Brake ().
-Whenever Accelerate () is called, increase the speed by 5, and whenever Brake () is called, decrease the speed by 5.
-To allow users to specify the speed to increase (or decrease), create two overloading methods for Accelerate () and Brake ()
+1. Create an Automobile class for a dealership.
+2. Include fields for an ID number, make, model, color, year, vin number, miles per gallon, and speed.
+3. Include get and set methods for each field.
+4. Do not allow the ID to be negative or more than 9999; if it is, set the ID to 0.
+5. Do not allow the year to be earlier than 2000 or later than 2017; if it is, set the year to 0.
+6. Do not allow the miles per gallon to be less than 10 or more than 60; if it is, set the miles per gallon to 0.
+7. Car speed should be initialized as 0.
+8. Include a constructor that accepts arguments for each field value and uses the set methods to assign the values.
+9. Also write two methods, Accelerate () and Brake ().
+10. Whenever Accelerate () is called, increase the speed by 5, and whenever Brake () is called, decrease the speed by 5.
+11. To allow users to specify the speed to increase (or decrease), create two overloading methods for Accelerate () and Brake ()
     that accept a single parameter specifying the increasing (or decreasing) speed.
-Write an application that declares several Automobile objects and demonstrates that all the methods work correctly.
-Save the files as Automobile.java and TestAutomobiles.java.
+12. Write an application that declares several Automobile objects and demonstrates that all the methods work correctly.
+13. Save the files as Automobile.java and TestAutomobiles.java.
 */
 
 package jaramillo.Ch5;
@@ -27,7 +27,7 @@ public class Automobile {
     private int year;
     private int vinNumber;
     private int mpg;
-    private int speed;
+    private int speed = 0;
 
     //get and set methods
     public int getIdNumber() {
@@ -63,7 +63,11 @@ public class Automobile {
         return year;
     }
     public void setYear(int setYear) {
-        year = setYear;
+        if (setYear >=2000 && setYear<= 2017) {
+            year = setYear;
+        } else {
+            year = 0;
+        }
     }
     public int getVin() {
         return vinNumber;
@@ -75,12 +79,43 @@ public class Automobile {
         return mpg;
     }
     public void setMpg(int setMpg) {
-        mpg = setMpg;
+        if (setMpg >= 10 & setMpg <= 60) {
+            mpg = setMpg;
+        } else {
+            mpg = 0;
+        }
     }
     public int getSpeed() {
         return speed;
     }
     public void setSpeed(int setSpeed) {
         speed = setSpeed;
+    }
+
+    // Constructors
+    public Automobile(int idNumber, String make, String model, String color, int year, int vinNumber, int mpg, int speed) {
+        setIdNumber(idNumber);
+        setMake(make);
+        setModel(model);
+        setColor(color);
+        setYear(year);
+        setVin(vinNumber);
+        setMpg(mpg);
+        setSpeed(speed);
+    }
+
+    // display method
+    public void displayAutomobile() {
+        System.out.println("\nID Number: " + this.idNumber + "\nMake: " + this.make + "\nModel: " + this.model + "\nColor: " + this.color + "\nYear: " + this.year + "\nVIN: " + this.vinNumber + "\nMPG: " + this.mpg + "\nCurrent Speed: " + this.speed);
+    }
+
+    // Accelerate and Break Methods
+    public String accelerate() {
+        speed =+ 5;
+        return "Speed is now " + speed + "mpg";
+    }
+    public String brake() {
+        speed -= 5;
+        return "Speed is now " + speed + "mpg";
     }
 }
--- jaramillo/Ch5/TestAutomobiles.java
+++ jaramillo/Ch5/TestAutomobiles.java
@@ -1,22 +1,49 @@
 /*
-Create an Automobile class for a dealership.
-Include fields for an ID number, make, model, color, year, vin number, miles per gallon, and speed.
-Include get and set methods for each field.
-Do not allow the ID to be negative or more than 9999; if it is, set the ID to 0.
-Do not allow the year to be earlier than 2000 or later than 2017; if it is, set the year to 0.
-Do not allow the miles per gallon to be less than 10 or more than 60; if it is, set the miles per gallon to 0.
-Car speed should be initialized as 0.
-Include a constructor that accepts arguments for each field value and uses the set methods to assign the values.
-Also write two methods, Accelerate () and Brake ().
-Whenever Accelerate () is called, increase the speed by 5, and whenever Brake () is called, decrease the speed by 5.
-To allow users to specify the speed to increase (or decrease), create two overloading methods for Accelerate () and Brake ()
+1. Create an Automobile class for a dealership.
+2. Include fields for an ID number, make, model, color, year, vin number, miles per gallon, and speed.
+3. Include get and set methods for each field.
+4. Do not allow the ID to be negative or more than 9999; if it is, set the ID to 0.
+5. Do not allow the year to be earlier than 2000 or later than 2017; if it is, set the year to 0.
+6. Do not allow the miles per gallon to be less than 10 or more than 60; if it is, set the miles per gallon to 0.
+7. Car speed should be initialized as 0.
+8. Include a constructor that accepts arguments for each field value and uses the set methods to assign the values.
+9. Also write two methods, Accelerate () and Brake ().
+10. Whenever Accelerate () is called, increase the speed by 5, and whenever Brake () is called, decrease the speed by 5.
+11. To allow users to specify the speed to increase (or decrease), create two overloading methods for Accelerate () and Brake ()
     that accept a single parameter specifying the increasing (or decreasing) speed.
-Write an application that declares several Automobile objects and demonstrates that all the methods work correctly.
-Save the files as Automobile.java and TestAutomobiles.java.
+12. Write an application that declares several Automobile objects and demonstrates that all the methods work correctly.
+13. Save the files as Automobile.java and TestAutomobiles.java.
 */
 
 package jaramillo.Ch5;
 
+import java.util.Scanner;
+
 public class TestAutomobiles {
-    
+    public static void main(String[] args) {
+        
+        // Scanner method
+        Scanner input = new Scanner(System.in);
+
+        // Creating Automobile Objects
+        Automobile carOne = new Automobile(21, "Nissan", "Sentra", "Red", 2010, 212121, 35, 0);
+        Automobile carTwo = new Automobile(-21, "Toyota", "RAV4", "White", 1999, 17381996, 5, 0);
+        Automobile carThree = new Automobile(10001, "Ford", "F-350", "Black", 2018, 123456789, 61, 0);
+        Automobile carFour = new Automobile(9998, "Buick", "Encore  ", "Blue", 2017, 987654321, 60, 0);
+
+        // Testing Methods
+        carOne.displayAutomobile();
+        carTwo.displayAutomobile();
+        carThree.displayAutomobile();
+        carFour.displayAutomobile();
+
+
+
+
+
+
+
+
+        input.close();
+    }
 }
