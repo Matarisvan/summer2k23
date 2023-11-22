2070c5e90eed6fd1112eda55da072fba2f9d1e14
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-11-22 16:46:04 -0500

    init_assignment

---
+++ jaramillo/Ch6/Population_for.java
@@ -0,0 +1,18 @@
+/*
+1. Assume that the population of Mexico is 121 million and that the population increases 1.05% annually (new population = current population x 1.0105). 
+2. Assume that the population of the United States is 315 million and that the population is reduced 0.16% annually (new population = current population x 0.9984) . 
+3. Assume that the population of Japan is 127 million and that the population increases 1.01% annually (new population = current population x 1.0101). 
+4. Write an application that displays the populations for the three countries every year until both Mexican and Japanese populations pass US population.
+5. Display the number of years it took for Mexico’s and Japan’s populations to exceed that of the United States. 
+6. Use both while loop and for loop to accomplish the task.
+7. Save the two files (one for each type of loop) as Population_while.java and Population_for.java
+
+Zaguate - Please submit the source code for your program along with a word document that includes screen shots of the compiled results and executed output.
+*/
+
+
+package jaramillo.Ch6;
+
+public class Population_for {
+    
+}
