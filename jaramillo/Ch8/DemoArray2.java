7c8ba577df2f2f1ff0f4709aab156da97357bc56
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-12-07 16:53:06 -0500

    init and populating array

---
+++ jaramillo/Ch8/DemoArray2.java
@@ -0,0 +1,13 @@
+package jaramillo.Ch8;
+
+public class DemoArray2 {
+    public static void main(String[] args) {
+
+        // initializing and populating an array
+        double[] salaries = {12.25, 13.55, 14.25, 16.85};
+        
+
+        System.out.println("\nSalaries one by one");
+        System.out.println(salaries[0] + "\n" + salaries[1] + "\n" + salaries[2] + "\n" + salaries[3]);
+    }
+}
